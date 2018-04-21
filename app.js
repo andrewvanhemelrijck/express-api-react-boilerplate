422503d9c29453db59f307f098e936c1454d29dd
pigeontoe <electriclady1@gmail.com>
2018-04-20 20:42:34 -0400

    set up express api

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 
 import index from './routes/index';
-import users from './routes/users';
 
 const app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// set up routes
+app.use('/api', index);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -34,9 +32,9 @@ app.use((err, req, res, next) => {
   // render the error page
   res.status(err.status || 500);
   res.send({
-    "error": {
-      "status": err.status || 500,
-      "message": err.message
+    error: {
+      status: err.status || 500,
+      message: err.message
     }
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+/* GET api/index route. */
+router.get('/', (req, res, next) => {
+  res.send({
+    title: 'Express API' 
+  });
 });
 
-module.exports = router;
+export default router;
