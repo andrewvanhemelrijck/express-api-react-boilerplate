a7d623991c0126389ba8a3eb76311c2748499cec
pigeontoe <electriclady1@gmail.com>
2018-04-26 22:02:24 -0400

    move around react bundle and fix gitignore

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
+    path: path.resolve(__dirname, 'dist/js/react'),
     filename: 'bundle.js',
   },
   module : {
